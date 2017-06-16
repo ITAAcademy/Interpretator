163f59370dcec4137ab1d60768d16719f23e6ff9
nico13051995 <nico13051995@gmail.com>
2017-06-16 20:52:03 +0300

    //add show err if cant close app

--- project/src/cLang.cpp
+++ project/src/cLang.cpp
@@ -647,61 +647,61 @@ char* LangCompiler::getSystemOutput(char* cmd){
 #define WRITE  1
 FILE * popen2(string command, string type, int & pid)
 {
-    pid_t child_pid;
-    int fd[2];
-    pipe(fd);
+	pid_t child_pid;
+	int fd[2];
+	pipe(fd);
 
-    if((child_pid = fork()) == -1)
-    {
-        perror("fork");
-        exit(1);
-    }
+	if((child_pid = fork()) == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
 
-    /* child process */
-    if (child_pid == 0)
-    {
-        if (type == "r")
-        {
-            close(fd[READ]);    //Close the READ end of the pipe since the child's fd is write-only
-            dup2(fd[WRITE], 1); //Redirect stdout to pipe
-        }
-        else
-        {
-            close(fd[WRITE]);    //Close the WRITE end of the pipe since the child's fd is read-only
-            dup2(fd[READ], 0);   //Redirect stdin to pipe
-        }
+	/* child process */
+	if (child_pid == 0)
+	{
+		if (type == "r")
+		{
+			close(fd[READ]);    //Close the READ end of the pipe since the child's fd is write-only
+			dup2(fd[WRITE], 1); //Redirect stdout to pipe
+		}
+		else
+		{
+			close(fd[WRITE]);    //Close the WRITE end of the pipe since the child's fd is read-only
+			dup2(fd[READ], 0);   //Redirect stdin to pipe
+		}
 
-        setpgid(child_pid, child_pid); //Needed so negative PIDs can kill children of /bin/sh
-        execl("/bin/sh", "/bin/sh", "-c", command.c_str(), NULL);
-        exit(0);
-    }
-    else
-    {
-        if (type == "r")
-        {
-            close(fd[WRITE]); //Close the WRITE end of the pipe since parent's fd is read-only
-        }
-        else
-        {
-            close(fd[READ]); //Close the READ end of the pipe since parent's fd is write-only
-        }
-    }
+		setpgid(child_pid, child_pid); //Needed so negative PIDs can kill children of /bin/sh
+		execl("/bin/sh", "/bin/sh", "-c", command.c_str(), NULL);
+		exit(0);
+	}
+	else
+	{
+		if (type == "r")
+		{
+			close(fd[WRITE]); //Close the WRITE end of the pipe since parent's fd is read-only
+		}
+		else
+		{
+			close(fd[READ]); //Close the READ end of the pipe since parent's fd is write-only
+		}
+	}
 
-    pid = child_pid;
+	pid = child_pid;
 
-    if (type == "r")
-    {
-        return fdopen(fd[READ], "r");
-    }
+	if (type == "r")
+	{
+		return fdopen(fd[READ], "r");
+	}
 
-    return fdopen(fd[WRITE], "w");
+	return fdopen(fd[WRITE], "w");
 }
 
 int pclose2(FILE * fp, pid_t pid)
 {
-    int stat;
+	int stat;
 
-    fclose(fp);
+	fclose(fp);
 	/*std::clock_t    start;
 	start = std::clock();
 
@@ -716,14 +716,28 @@ int pclose2(FILE * fp, pid_t pid)
     }
 
     return stat;*/
-    	int killResult =  kill (pid + 1, SIGKILL);
-    	INFO("close: pid:" +  to_string(pid)  + " killResult:" +  to_string(killResult));
-    	if(killResult != 0)
-    	{
-    		ERROR("Couldn't kill process");
-    	}
-
-    return 0;
+	int killResult =  kill(pid + 1, SIGKILL);
+	if(killResult != 0)
+	{
+		killResult = errno;
+		string err = "Couldn't kill process.";
+		switch(killResult ){
+		case EINVAL:
+			ERROR(err + "Invalid argument!!!");
+			break;
+		case ESRCH:
+			//ERROR("Process not found!!!");
+			break;
+		case EPERM:
+			ERROR(err + "Operation not permitted!!!");
+			break;
+		default:
+			ERROR(err + to_string(killResult));
+			break;
+		}
+	}
+	INFO("close: pid:" +  to_string(pid)  + " killResult:" +  to_string(killResult));
+	return killResult;
 }
 
 string LangCompiler::getStdoutFromCommand(string cmd, int mTimeOut, long double *executionTime)
--- project/src/inc/cLang.h
+++ project/src/inc/cLang.h
@@ -6,6 +6,7 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <errno.h>
 #include <sys/wait.h>
 #include "logfile.h"
 #include <string>
